7ee47cdc4d3aa93b71dce5b3e8dc959fe1ac7b1f
extremera89 <99046475+extremera89@users.noreply.github.com>
2022-05-07 20:57:58 +0200

    Implementando modelo Exposicion y modelo Visita

--- src/modelo/Exposicion.java
+++ src/modelo/Exposicion.java
@@ -1,4 +1,95 @@
 package modelo;
 
+import java.sql.Date;
+
 public class Exposicion {
+
+    private String nombre;
+    private String tematica;
+    private Date fechainicio;
+    private Date fechafin;
+    private String descripcion;
+    private int numsala;
+    //private Sala sala;
+
+
+    public Exposicion() {
+    }
+
+    public Exposicion(String nombre, String tematica, Date fechainicio, Date fechafin, String descripcion, int numsala) {
+        this.nombre = nombre;
+        this.tematica = tematica;
+        this.fechainicio = fechainicio;
+        this.fechafin = fechafin;
+        this.descripcion = descripcion;
+        this.numsala = numsala;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTematica() {
+        return tematica;
+    }
+
+    public void setTematica(String tematica) {
+        this.tematica = tematica;
+    }
+
+    public Date getFechainicio() {
+        return fechainicio;
+    }
+
+    public void setFechainicio(Date fechainicio) {
+        this.fechainicio = fechainicio;
+    }
+
+    public Date getFechafin() {
+        return fechafin;
+    }
+
+    public void setFechafin(Date fechafin) {
+        this.fechafin = fechafin;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getNumsala() {
+        return numsala;
+    }
+
+    public void setNumsala(int numsala) {
+        this.numsala = numsala;
+    }
+
+    /*public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Exposicion{" +
+                "nombre='" + nombre + '\'' +
+                ", tematica='" + tematica + '\'' +
+                ", fechainicio=" + fechainicio +
+                ", fechafin=" + fechafin +
+                ", descripcion='" + descripcion + '\'' +
+                ", numsala=" + numsala +
+                '}';
+    }
 }
--- src/modelo/VisitaGuiada.java
+++ src/modelo/VisitaGuiada.java
@@ -1,4 +1,83 @@
 package modelo;
 
+import java.util.Date;
+
 public class VisitaGuiada {
+    private int numvisita;
+    private int numpersonas;
+    private Date fecha;
+    private String centro;
+    private String dnimonitor;
+    //private Monitor monitor;
+
+
+    public VisitaGuiada() {
+    }
+
+    public VisitaGuiada(int numvisita, int numpersonas, Date fecha, String centro, String dnimonitor) {
+        this.numvisita = numvisita;
+        this.numpersonas = numpersonas;
+        this.fecha = fecha;
+        this.centro = centro;
+        this.dnimonitor = dnimonitor;
+    }
+
+    public int getNumvisita() {
+        return numvisita;
+    }
+
+    public void setNumvisita(int numvisita) {
+        this.numvisita = numvisita;
+    }
+
+    public int getNumpersonas() {
+        return numpersonas;
+    }
+
+    public void setNumpersonas(int numpersonas) {
+        this.numpersonas = numpersonas;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCentro() {
+        return centro;
+    }
+
+    public void setCentro(String centro) {
+        this.centro = centro;
+    }
+
+    public String getDnimonitor() {
+        return dnimonitor;
+    }
+
+    public void setDnimonitor(String dnimonitor) {
+        this.dnimonitor = dnimonitor;
+    }
+
+    /*public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }*/
+
+    @Override
+    public String toString() {
+        return "VisitaGuiada{" +
+                "numvisita=" + numvisita +
+                ", numpersonas=" + numpersonas +
+                ", fecha=" + fecha +
+                ", centro='" + centro + '\'' +
+                ", dnimonitor='" + dnimonitor + '\'' +
+                '}';
+    }
 }
