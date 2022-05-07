284c0e92c18fea05035b9fcc74f41f1dd718929d
fmesmes <fmesmes2308@g.educaand.es>
2022-05-07 21:50:15 +0200

    pequeña actualizacion base datos

--- src/principal/principal.java
+++ src/principal/principal.java
@@ -1,15 +1,16 @@
 package principal;
 
+import dao.DAOlogin;
 import modelo.Login;
 
 public class principal {
     public static void main(String[] args) {
-        DAO.LoginDAO lg=new DAO.LoginDAO();
+        DAOlogin lg=new DAOlogin();
 
         Login l1=new Login("juan","juan",1);
         //lg.insertarNuevoUsuario(l1);
         //lg.borrarUsuario("juan");
-        lg.cambiarContraseña("juan","paquito");
+        lg.cambiarContrasenia("juan","paquito");
         System.out.println(lg.comprobarExistenciaUsuario("juan"));
         lg.actualizarTipoUsuario("juan",0);
 
